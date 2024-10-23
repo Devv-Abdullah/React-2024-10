385fd6606f8a2f2d640a38ecac8bc6acf2032c8b
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-10-23 19:16:47 +0300

    lecture 1 react

--- client/store/src/App.js
+++ client/store/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+// import NewPage from './newPage';
+import ResponsiveAppBar from './nav.js'
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome to our react class
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ client/store/src/newPage.js
@@ -0,0 +1,12 @@
+// function component
+
+
+function NewPage(){
+    return ( // لازم يكون فيها اي تاغ ل html واسكره
+        // <div>
+            <p>i am component</p>
+        // </div>    
+    )
+}
+
+export default NewPage; 
