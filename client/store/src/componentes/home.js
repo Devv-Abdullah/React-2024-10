cb3c7994c2d2d801382064fe3d8ba26e36c744fc
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-11-03 17:43:00 +0300

    online lecture

--- client/store/src/componentes/home.js
+++ client/store/src/componentes/home.js
@@ -1,8 +1,23 @@
 import ButtonAppBar from "./navBar";
 import NewComponent from "./classComponent";
 import './style/home.css' //لربط css
+import { useState } from "react";
 
+// useEffect:
+// state حاله لقيمة متغير
 export default function Home(props){
+    var [counter, setCounter] = useState(0);
+    // increment counter
+    function increment(){
+        setCounter(counter+1);
+    }
+    // decrement
+    function decrement(){
+        console.log('before', counter);
+        setCounter(counter-1);
+        console.log('after', counter);
+    }
+    const newList = ["Item1", "Item2", "Item3"];
     return (
         <>
             <ButtonAppBar/>
@@ -11,6 +26,13 @@ export default function Home(props){
                 <h1 className="title" style = {{ textAlign:"center"}}>
                     Welcome to home page {props.name}
                 </h1>
+                <button onClick={decrement}>-</button>
+                <p>{counter}</p>
+                <button onClick={increment}>+</button>
+
+                {newList.map((item, index) =>(
+                <li key={index}>{item}</li>
+                ))}
             </div>
         </>
     )
