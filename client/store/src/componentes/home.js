c4644a42cadf1f276d846789172e5d5fca188e7d
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-11-05 20:16:15 +0300

    lecture 6 react js

--- client/store/src/componentes/home.js
+++ client/store/src/componentes/home.js
@@ -24,11 +24,11 @@ export default function Home(props){
     
     var allProducts =[
         {name: 'product1', price:100, category:'men', dis:'this is product 1'},
-        {name: 'product2', price:100, category:'Children', dis:'this is product 1'},
+        {name: 'product2', price:200, category:'Children', dis:'this is product 2'},
         {name: 'product3', price:100, category:'men', dis:'this is product 1'},
-        {name: 'product4', price:100, category:'Children', dis:'this is product 1'},
+        {name: 'product4', price:400, category:'Children', dis:'this is product 4'},
         {name: 'product5', price:100, category:'men', dis:'this is product 1'},
-        {name: 'product6', price:100, category:'Children', dis:'this is product 1'},
+        {name: 'product6', price:600, category:'Children', dis:'this is product 6'},
         {name: 'product7', price:100, category:'men', dis:'this is product 1'},
     ]
 
