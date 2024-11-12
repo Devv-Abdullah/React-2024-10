2141d680c2595b53e18d7236d2a0af8332b7a3c4
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-11-12 17:34:21 +0300

    answer tasks

--- client/store/src/componentes/news.js
+++ client/store/src/componentes/news.js
@@ -1,7 +1,19 @@
 import ButtonAppBar from "./navBar";
-
+//add form to add age and user name and very time you add these data if shown in a new card
+import {Card, CardContent, Typography} from '@mui/material'
+import { useState } from "react";
 
 export default function News(){
+    const [users, setUsers] = useState([]);
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const handelSubmit = (e) => {
+        e.preventDefault();
+        var user = {name:name, age:age};
+        setUsers([...users, user])
+        console.log(users);
+    }
+
     return(
     <>
     <ButtonAppBar/>
@@ -9,6 +21,27 @@ export default function News(){
         <h1 style = {{color:"black", backgroundColor:"yellow", textAlign:"center"}}>
             Welcome to new Page
         </h1>
+        <form id="userForm" onSubmit={handelSubmit}>
+            <label>Name: </label>
+            <input type="text" id="name" name="name" required value={name} onChange={(e)=>{
+                setName(e.target.value) //باشر على قيمة النيم والايج
+            }}/> <br/><br/>
+            <label>Age: </label>
+            <input type="number" id="age" name="age" required value={age} onChange={(e)=>{
+                setAge(e.target.value)
+            }}/>
+            <br/><br/>
+            <button type="submit">Submit</button>
+        </form>
+
+        {users.map((user, index)=>(
+            <Card key={index}>
+                <CardContent>
+                    <Typography>Name: {user.name}</Typography>
+                    <Typography>Age: {user.age}</Typography>
+                </CardContent>
+            </Card>
+        ))}
     </div>
     </>
     )
