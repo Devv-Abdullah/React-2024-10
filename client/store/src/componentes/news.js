e3d66c1ff0af99fb2dc3b744f9d7a1e7499b5f61
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-11-16 23:27:03 +0300

    lecture review

--- client/store/src/componentes/news.js
+++ client/store/src/componentes/news.js
@@ -4,13 +4,13 @@ import {Card, CardContent, Typography} from '@mui/material'
 import { useState } from "react";
 
 export default function News(){
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]); // عشان كل ما اضسف يوزر ينضاف لهاي اليست
     const [name, setName] = useState("");
-    const [age, setAge] = useState(0);
+    const [age, setAge] = useState(); // useState(0) 
     const handelSubmit = (e) => {
         e.preventDefault();
-        var user = {name:name, age:age};
-        setUsers([...users, user])
+        // var user = {name:name, age:age};
+        setUsers([...users, {name, age}]) //... => prevues value القيمه القديمه تبعت المتغير 
         console.log(users);
     }
 
@@ -21,10 +21,11 @@ export default function News(){
         <h1 style = {{color:"black", backgroundColor:"yellow", textAlign:"center"}}>
             Welcome to new Page
         </h1>
+        
         <form id="userForm" onSubmit={handelSubmit}>
             <label>Name: </label>
-            <input type="text" id="name" name="name" required value={name} onChange={(e)=>{
-                setName(e.target.value) //باشر على قيمة النيم والايج
+            <input type="text" id="name" name="name" required value={name} onChange={(e)=>{ // value => تبعت target
+                setName(e.target.value) //باشر على قيمة النوم والايج
             }}/> <br/><br/>
             <label>Age: </label>
             <input type="number" id="age" name="age" required value={age} onChange={(e)=>{
@@ -34,14 +35,16 @@ export default function News(){
             <button type="submit">Submit</button>
         </form>
 
-        {users.map((user, index)=>(
-            <Card key={index}>
+        {users.map((user, index)=>{ // card=> هو العنصر الرئيسي الذي يعمل كوعاء (Container) يحتوي على المحتوى , CardContent=>مكوّن يُستخدم لاحتواء العناصر الداخلية للبطاقة، مثل النصوص أو الصور 
+            return(
+            <Card key={index}> 
                 <CardContent>
                     <Typography>Name: {user.name}</Typography>
                     <Typography>Age: {user.age}</Typography>
                 </CardContent>
             </Card>
-        ))}
+            )
+            })}
     </div>
     </>
     )
